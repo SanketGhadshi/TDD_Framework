ed50703de73ceb259a9c0eb1f33dabc2dd51a552
sanketghsdhi96@gmail.com <sankighadshi@gmail.com>
2025-07-13 13:39:58 +0530

    This commit from asian timezone guy

---
+++ src/main/java/com/projectname/base/gitDemo.java
@@ -0,0 +1,13 @@
+package com.projectname.base;
+
+public class gitDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Demo change 1");
+		System.out.println("Demo change 2");
+
+
+	}
+
+}
